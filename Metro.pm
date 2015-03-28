12213d1f24cae4fe34b814297aeeeeb65b18a9b1
Michal Špaček <tupinek@gmail.com>
2015-03-28 17:56:57 +0100

    Move from Task::Map::Tube.

---
+++ Metro.pm
@@ -0,0 +1,78 @@
+package Task::Map::Tube::Metro;
+
+# Pragmas.
+use strict;
+use warnings;
+
+# Version.
+our $VERSION = 0.01;
+
+1;
+
+__END__
+
+=pod
+
+=encoding utf8
+
+=head1 NAME
+
+Task::Map::Tube - Install the Map::Tube concrete metro modules.
+
+=head1 SYNOPSIS
+
+ cpanm Task::Map::Tube::Metro
+
+=head1 SEE ALSO
+
+L<Map::Tube::Barcelona>,
+L<Map::Tube::Beijing>,
+L<Map::Tube::Berlin>,
+L<Map::Tube::Bucharest>,
+L<Map::Tube::Budapest>,
+L<Map::Tube::Delhi>,
+L<Map::Tube::Dnipropetrovsk>,
+L<Map::Tube::Glasgow>,
+L<Map::Tube::Kazan>,
+L<Map::Tube::Kharkiv>,
+L<Map::Tube::Kiev>,
+L<Map::Tube::KoelnBonn>,
+L<Map::Tube::London>,
+L<Map::Tube::Lyon>,
+L<Map::Tube::Minsk>,
+L<Map::Tube::Moscow>,
+L<Map::Tube::NYC>,
+L<Map::Tube::Nanjing>,
+L<Map::Tube::Novosibirsk>,
+L<Map::Tube::Prague>,
+L<Map::Tube::SaintPetersburg>,
+L<Map::Tube::Samara>,
+L<Map::Tube::Singapore>,
+L<Map::Tube::Sofia>,
+L<Map::Tube::Tbilisi>,
+L<Map::Tube::Tokyo>,
+L<Map::Tube::Vienna>,
+L<Map::Tube::Warsaw>,
+L<Map::Tube::Yekaterinburg>.
+
+=head1 REPOSITORY
+
+L<https://github.com/tupinek/Task-Map-Tube-Metro>
+
+=head1 AUTHOR
+
+Michal Špaček L<mailto:skim@cpan.org>
+
+L<http://skim.cz>
+
+=head1 LICENSE AND COPYRIGHT
+
+ © 2015 Michal Špaček
+ Artistic License
+ BSD 2-Clause License
+
+=head1 VERSION
+
+0.01
+
+=cut
